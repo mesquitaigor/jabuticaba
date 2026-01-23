25fce6a304d1d91604e6c073257bcdfdea06ca5c
mesquitaigor <igor.mesqbessa@gmail.com>
2026-01-23 17:25:37 -0300

    feat(header): implement header component with menu and drawer functionality

--- src/app/core/layout/header/header.ts
+++ src/app/core/layout/header/header.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+import { DrawerModule } from 'primeng/drawer';
 
 @Component({
   selector: 'jbt-header',
+  imports: [ButtonModule, AvatarModule, DrawerModule],
   templateUrl: './header.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public readonly menuOpen = signal(false);
+  public openMenu(): void {
+    this.menuOpen.set(!this.menuOpen());
+  }
+}
