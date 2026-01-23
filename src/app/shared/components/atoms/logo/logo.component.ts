a879e86d14621561befe22804b2ca212022ad9e3
mesquitaigor <igor.mesqbessa@gmail.com>
2026-01-23 18:05:49 -0300

    feat(logo): add LogoComponent with basic template and properties

---
+++ src/app/shared/components/atoms/logo/logo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jbt-logo.component',
+  imports: [],
+  template: '<img [src]="logoSrc" [alt]="logoAlt" />',
+})
+export class LogoComponent {
+  public readonly logoAlt = 'Jabuticaba app';
+  public readonly logoSrc = 'icons/logo.svg';
+}
