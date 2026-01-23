abff92db1420579f88661dd272dccaeb34bcfef9
mesquitaigor <igor.mesqbessa@gmail.com>
2026-01-23 17:03:39 -0300

    feat(grocery-list): add grocery list component and routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'grocery-list',
+    loadComponent: () =>
+      import('./modules/grocery-list/grocery-list.component').then(
+        (m) => m.GroceryListComponent,
+      ),
+  },
+];
---
+++ src/app/modules/grocery-list/grocery-list.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jbt-grocery-list',
+  templateUrl: './grocery-list.component.html',
+})
+export class GroceryListComponent {}
