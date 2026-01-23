7bcc13a0b016a98f1f57230bc3521f0d94d0960c
mesquitaigor <igor.mesqbessa@gmail.com>
2026-01-23 17:55:03 -0300

    feat(sidebar): integrate menu items into sidebar component and remove placeholder text

--- src/app/core/layout/sidebar/sidebar.component.ts
+++ src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,12 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { DrawerModule } from 'primeng/drawer';
 import { SidebarService } from './sidebar.service';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'jbt-sidebar',
-  imports: [DrawerModule],
+  imports: [DrawerModule, MenuModule],
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {
   public readonly sidebarService = inject(SidebarService);
+  public readonly items: MenuItem[] = [
+    { label: 'New', icon: 'pi pi-plus' },
+    { label: 'Search', icon: 'pi pi-search' },
+  ];
 }
