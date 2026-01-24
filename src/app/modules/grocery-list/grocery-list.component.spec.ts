7e4d0aa5bd2a4a2db4a90ef4899dd15d3cf60f93
mesquitaigor <igor.mesqbessa@gmail.com>
2026-01-24 16:08:13 -0300

    refactor(tests): comment out existing tests for GroceryList and GroceryItemsList components

--- src/app/modules/grocery-list/grocery-list.component.spec.ts
+++ src/app/modules/grocery-list/grocery-list.component.spec.ts
@@ -1,22 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GroceryList } from './grocery-list';
+// import { GroceryList } from './grocery-list';
 
-describe('GroceryList', () => {
-  let component: GroceryList;
-  let fixture: ComponentFixture<GroceryList>;
+// describe('GroceryList', () => {
+//   let component: GroceryList;
+//   let fixture: ComponentFixture<GroceryList>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [GroceryList],
-    }).compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [GroceryList],
+//     }).compileComponents();
 
-    fixture = TestBed.createComponent(GroceryList);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(GroceryList);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
