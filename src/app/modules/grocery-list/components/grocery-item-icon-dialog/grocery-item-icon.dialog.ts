17d1282c1eb0c448e48f32c9608065730eb1e9fd
mesquitaigor <igor.mesqbessa@gmail.com>
2026-02-12 21:54:47 -0300

    feat(grocery-item-icon): add grocery item icon dialog component with template and tests

---
+++ src/app/modules/grocery-list/components/grocery-item-icon-dialog/grocery-item-icon.dialog.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jbt-grocery-item-icon-dialog',
+  imports: [],
+  templateUrl: './grocery-item-icon.dialog.html',
+})
+export class GroceryItemIconDialog {}
