179da7e01fcc4ad0c5b02a5ad6c21433b01b5a31
mesquitaigor <igor.mesqbessa@gmail.com>
2026-02-14 11:24:36 -0300

    fix(environment): update supabaseUrl and supabaseKey for environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://rdyokmnmwelrfjptlmbo.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkeW9rbW5td2VscmZqcHRsbWJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NjU5NDIsImV4cCI6MjA3ODU0MTk0Mn0.WNXe2N-hUlN_R6fjPt8eDvo9T3-VU8BE8l09eNmRGQQ',
+  supabaseUrl: 'https://vwjzpmpwhpoozbtfueqf.supabase.co',
+  supabaseKey: 'sb_publishable_wqnzLg6cmvMRBmIIR6r6bw_qHNxplTV',
 };
